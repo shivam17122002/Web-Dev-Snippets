3546f1a6ac6865178c1bc2abdf69d309ce900eef
shivam17122002 <shivmpandey02@gmail.com>
2025-08-17 16:00:05 +0530

    push snippets

---
+++ brain.js
@@ -0,0 +1,80 @@
+// class Human{
+//    #salary = 120000;
+//    age = 13;
+//    weight = 74;
+//    height = 180;
+
+//    walking(){
+//       console.log("I am walking")
+//    }
+
+//    running(){
+//       console.log("I am running")
+//    }
+
+//    get fetchSalary() {
+//       return this.#salary;
+//    }
+
+//    set updateSalary(val){
+//       this.#salary = val;
+//    }
+   
+// }
+
+
+// let  obj = new Human();
+// obj.updateSalary = 23000;
+
+// console.log(obj.fetchSalary);
+
+// function sayName(fName = "Po", lName = "La"){
+//    console.log("My name is: ", fName, "", lName);
+// }
+
+// sayName("shivam", "pandey");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(34,532,90,2,934,));
+
+// let  num = Math.PI;
+
+// console.log(Math.floor(num));
+// console.log(Math.round(32.424));
+
+// console.log(Math.random());
+
+// let src = {
+//     age: 12,
+//     weight: 78,
+//     height: 230
+// };
+
+// let dest = {};
+
+// for(let key in src){
+//     dest[key] = src[key];
+// }
+
+// src.age = 90;
+
+// console.log("src: ", src);
+// console.log("dest: ", dest);
+
+try{
+    console.log(x);
+}
+
+catch(err){
+    throw new Error("Invalid JSON");
+}
+
+
+
+
+
+
+
+
+
