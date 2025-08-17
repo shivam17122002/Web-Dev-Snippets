096259a71dc90269d274e55ed0afec2cbd965052
shivam17122002 <shivmpandey02@gmail.com>
2025-08-17 17:06:18 +0530

    DOM Manuplation

--- brain.js
+++ brain.js
@@ -1,74 +1,89 @@
-class Human{
-   #salary = 120000;
-   age = 13;
-   weight = 74;
-   height = 180;
+// class Human{
+//    #salary = 120000;
+//    age = 13;
+//    weight = 74;
+//    height = 180;
 
-   walking(){
-      console.log("I am walking")
-   }
+//    walking(){
+//       console.log("I am walking")
+//    }
 
-   running(){
-      console.log("I am running")
-   }
+//    running(){
+//       console.log("I am running")
+//    }
 
-   get fetchSalary() {
-      return this.#salary;
-   }
+//    get fetchSalary() {
+//       return this.#salary;
+//    }
 
-   set updateSalary(val){
-      this.#salary = val;
-   }
+//    set updateSalary(val){
+//       this.#salary = val;
+//    }
    
-}
+// }
 
 
-let  obj = new Human();
-obj.updateSalary = 23000;
+// let  obj = new Human();
+// obj.updateSalary = 23000;
 
-console.log(obj.fetchSalary);
+// console.log(obj.fetchSalary);
 
-function sayName(fName = "Po", lName = "La"){
-   console.log("My name is: ", fName, "", lName);
-}
+// function sayName(fName = "Po", lName = "La"){
+//    console.log("My name is: ", fName, "", lName);
+// }
 
-sayName("shivam", "pandey");
+// sayName("shivam", "pandey");
 
-console.log(Math.PI);
+// console.log(Math.PI);
 
-console.log(Math.max(34,532,90,2,934,));
+// console.log(Math.max(34,532,90,2,934,));
 
-let  num = Math.PI;
+// let  num = Math.PI;
 
-console.log(Math.floor(num));
-console.log(Math.round(32.424));
+// console.log(Math.floor(num));
+// console.log(Math.round(32.424));
 
-console.log(Math.random());
+// console.log(Math.random());
 
-let src = {
-    age: 12,
-    weight: 78,
-    height: 230
-};
+// let src = {
+//     age: 12,
+//     weight: 78,
+//     height: 230
+// };
 
-let dest = {};
+// let dest = {};
 
-for(let key in src){
-    dest[key] = src[key];
-}
+// for(let key in src){
+//     dest[key] = src[key];
+// }
 
-src.age = 90;
+// src.age = 90;
 
-console.log("src: ", src);
-console.log("dest: ", dest);
+// console.log("src: ", src);
+// console.log("dest: ", dest);
 
-try{
-    console.log(x);
-}
+// try{
+//     console.log(x);
+// }
 
-catch(err){
-    throw new Error("Invalid JSON");
-}
+// catch(err){
+//     throw new Error("Invalid JSON");
+// }
+
+
+let secondElement = document.getElementById('sdiv');
+console.log(paraElement);
+
+secondElement.style.cssText;
+
+secondElement.style.cssText = "backgorund-color: black; color: white; padding: 1.2rem";
+
+let firstElement = document.querySelector('#fdiv');
+console.log(firstElement);
+firstElement.setAttribute('class',  'divClass');
+console.log(firstElement);
+
+firstElement.setAttribute('style',"padding: 0.1rem");
 
 
 
